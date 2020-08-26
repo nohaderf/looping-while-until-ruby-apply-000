bfd6fcbdf8593c33069e1b4864e25b10f22d1c43
Freda Hon <freda.hon@gmail.com>
2020-08-26 00:04:13 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -5,8 +5,3 @@ def using_until
     levitation_force += 1
   end
 end
-
-# I initially put until levitation_force = 10, but that didn't work
-# Changed it to >11, that didn't work either
-# Last switched it to >9.
-# Note: Until loop will still run the loop one more time and stop only af
